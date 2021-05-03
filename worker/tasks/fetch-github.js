443669dc72ea1bd265172e260699e51ee7a8172c
Prathamesh-Shanbhag <prathameshshanbhag074@gmail.com>
2021-05-03 16:42:08 +0530

    Promisify Redis

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -1,7 +1,9 @@
 var fetch = require('node-fetch');
 const redis = require('redis');
 const client = redis.createClient();
-
+const { promisify } = require('util');
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.get).bind(client);
 const baseURL = 'https://jobs.github.com/positions.json';
 
 async function fetchGithub() {
