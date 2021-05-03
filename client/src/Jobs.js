bf1f662951e41f071236bbecf4c8ccc790a7fac1
Prathamesh-Shanbhag <prathameshshanbhag074@gmail.com>
2021-05-03 16:25:49 +0530

    Created Jobs Component

---
+++ client/src/Jobs.js
@@ -0,0 +1,13 @@
+import Typography from '@material-ui/core/Typography';
+import Job from './Job';
+
+export default function Jobs({ jobs }) {
+  return (
+    <div className='jobs'>
+      <Typography variant='h1'>Entry Level Software Jobs</Typography>
+      {jobs.map((job) => (
+        <Job job={job} />
+      ))}
+    </div>
+  );
+}
