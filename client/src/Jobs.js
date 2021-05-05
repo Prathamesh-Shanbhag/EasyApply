c281cfc5d801ba0c30886e7f86cda9e6eedbd923
Prathamesh-Shanbhag <prathameshshanbhag074@gmail.com>
2021-05-05 14:16:21 +0530

    Added Stepper Pagination

--- client/src/Jobs.js
+++ client/src/Jobs.js
@@ -1,15 +1,61 @@
 import Typography from '@material-ui/core/Typography';
 import Job from './Job';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import { useState } from 'react';
 
 export default function Jobs({ jobs }) {
+  const numJobs = jobs.length;
+  const numPages = Math.ceil(numJobs / 50);
+
+  const [activeStep, setActiveStep] = useState(0);
+  const jobsOnPage = jobs.slice(activeStep * 50, activeStep * 50 + 50);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
   return (
     <div className='jobs'>
       <Typography variant='h4' component='h1'>
         Entry Level Software Jobs
       </Typography>
-      {jobs.map((job) => (
-        <Job job={job} />
+      <Typography variant='h6' component='h2'>
+        Found {numJobs} Jobs.
+      </Typography>
+      {jobsOnPage.map((job, i) => (
+        <Job key={i} job={job} />
       ))}
+      <div>
+        Page {activeStep + 1} of {numPages}
+      </div>
+      <MobileStepper
+        variant='progress'
+        steps={numPages}
+        position='static'
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size='small'
+            onClick={handleNext}
+            disabled={activeStep === numPages - 1}
+          >
+            Next
+            {<KeyboardArrowRight />}
+          </Button>
+        }
+        backButton={
+          <Button size='small' onClick={handleBack} disabled={activeStep === 0}>
+            {<KeyboardArrowLeft />}
+            Back
+          </Button>
+        }
+      />
     </div>
   );
 }
